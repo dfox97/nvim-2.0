44349cc119f3068b5e6c8b8fa96e71682a95488c
Daniel Fox <61083107+dfox97@users.noreply.github.com>
2024-01-16 21:42:49 +0000

    Add files via upload

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,7 +1,11 @@
 require('telescope').setup({ file_ignore_patterns = { "node%_modules/.*" } })
-local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
+local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
-vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ps', function()
+  vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
